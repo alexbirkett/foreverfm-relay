6c763fb160e772c115a6055952b8ea15e2ae4af1
Alex Birkett <alex@birkett.no>
2012-12-05 19:32:19 +0000

    @psobot's local changes checked in by alex

--- relay.js
+++ relay.js
@@ -33,18 +33,6 @@ fs.readFile('./crossdomain.xml', function(error, content) {
     if (!error) crossdomain = content;
 });
 
-var __transfer_exceeded = false;
-var transfer_exceeded = function() {
-    cur_month = (new Date()).getMonth();
-    if (stats.month != cur_month) {
-        stats.month = cur_month;
-        stats.bytes_out_month = 0;
-        stats.bytes_in_month = 0;
-    }
-    __transfer_exceeded = stats.bytes_out_month > config.max_monthly_transfer;
-    return __transfer_exceeded;
-}
-
 var logger = new (winston.Logger)({
     transports: [
         new winston.transports.Console(
@@ -90,28 +78,15 @@ var listen = function(callback) {
         } else {
             logger.info("Listening to generator!")
             res.on('data', function (buf) {
-                try {
-                    stats.bytes_in_month += buf.length
-                    for (l in listeners) {
-                        listeners[l].write(buf);
-                        stats.bytes_out_month += buf.length;
-                        if (stats.peaks.bytes_out_month < stats.bytes_out_month)
-                            stats.peaks.bytes_out_month = stats.bytes_out_month;
-                    }
-                    if ( __transfer_exceeded ) {
-                        logger.error("Maximum uplink exceeded! Shutting off.");
-                        for (l in listeners) listeners[l].end();
-                        req.destroy();
-                    }
-                } catch (err) {
-                    logger.error("Could not send to listeners: " + err);
-                }
+	    	stats.bytes_in_month += buf.length
+	   	for (l in listeners) {
+		    listeners[l].write(buf);
+		    stats.bytes_out_month += buf.length;
+	        }
             });
             res.on('end', function () {
-                if ( !transfer_exceeded() ) {
-                    logger.error("Stream ended! Restarting listener...");
-                    setTimeout(function(){listen(function(){})}, config.timeout);
-                }
+                logger.error("Stream ended! Restarting listener...");
+                setTimeout(function(){listen(function(){})}, config.timeout);
             });
             if (typeof callback != "undefined") callback();
         }
@@ -133,37 +108,13 @@ var ipof = function(req) {
 };
 
 var available = function(response) {
-    if ( transfer_exceeded() ) {
-        logger.error("Max transfer exceeded: returning 301.");
-        response.writeHead(301, {'Location': "http://" + options.hostname + options.path})
-        response.end();
-        return false;
-    }
     if ( listeners.length + 1 > config.listener_limit ) {
-        logger.error("Listener limit exceeded: returning 503.");
-        response.writeHead(503);
+        logger.error("Listener limit exceeded: returning 301 to relay01.");
+        response.writeHead(301, {'Location': "http://relay01.forever.fm/all.mp3"});
         response.end();
         return false;
     } 
     return true;
-}
-
-var prune = function() {
-    limit = (+new Date) - config.heartbeat_interval;
-    remove = [];
-    for (l in listeners) {
-        if (listeners[l].last_heartbeat && listeners[l].last_heartbeat < limit) {
-            remove.push(listeners[l]);
-        }
-    }
-    if (remove.length > 0)
-        logger.info("Removing " + remove.length + " listeners due to heartbeat failure.");
-
-    for (r in remove) {
-        remove[r].end();
-        logger.info("Forcibly removing listener: " + remove[r].ip);
-        listeners.splice(listeners.indexOf(remove[r]), 1);
-    }
 }
 
 var save = function() {
@@ -176,7 +127,6 @@ var save = function() {
 var run = function() {
     logger.info("Starting server.")
 
-    if ( config.heartbeat_required ) setInterval( prune, config.heartbeat_interval );
     setInterval( save, config.save_interval );
 
     http.createServer(function(request, response) {
@@ -208,6 +158,8 @@ var run = function() {
                     }
                     break;
                 case "/":
+		    if (stats.peaks.bytes_out_month < stats.bytes_out_month)
+		        stats.peaks.bytes_out_month = stats.bytes_out_month;
                     response.write(JSON.stringify({
                         listeners: listeners.length,
                         bytes_in_month: stats.bytes_in_month,
@@ -224,14 +176,6 @@ var run = function() {
                     response.end();
                     break;
                 default:
-                    if ( config.heartbeat_required ) {
-                        for (l in listeners) {
-                            if (listeners[l].ip == request.ip) {
-                                listeners[l].last_heartbeat = (+new Date);
-                                break;
-                            }
-                        }
-                    }
                     response.writeHead(200);
                     response.end();
                     break;
